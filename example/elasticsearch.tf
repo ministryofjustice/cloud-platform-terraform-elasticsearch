35ec59ab32eb9a06b8c3aacc9e05e501c4864cbe
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2019-11-07 20:11:40 +0000

    working version with assumerole

--- example/elasticsearch.tf
+++ example/elasticsearch.tf
@@ -15,16 +15,31 @@ variable "cluster_state_bucket" {}
  *
  */
 module "example_team_es" {
-  source               = "../"
-  cluster_name         = "${var.cluster_name}"
-  cluster_state_bucket = "${var.cluster_state_bucket}"
-  team_name            = "example-repo"
-  business-unit        = "example-bu"
-  application          = "exampleapp"
-  is-production        = "false"
-  environment-name     = "dev"
+  source                 = "../"
+  cluster_name           = "${var.cluster_name}"
+  cluster_state_bucket   = "${var.cluster_state_bucket}"
+  team_name              = "example-repo"
+  business-unit          = "example-bu"
+  application            = "exampleapp"
+  is-production          = "false"
+  environment-name       = "dev"
   infrastructure-support = "cloud-platform"
-  elasticsearch-domain = "example-es"
+  elasticsearch-domain   = "example-es"
+  namespace              = "poornima-dev"
+}
 
+resource "kubernetes_secret" "example_team_es" {
+  metadata {
+    name      = "example-team-es-cluster-output"
+    namespace = "poornima-dev"
+  }
 
+  data {
+    es_domain_arn   = "${module.example_team_es.domain_arn}"
+    domain_id       = "${module.example_team_es.domain_id}"
+    domain_endpoint = "${module.example_team_es.domain_endpoint}"
+    kibana_endpoint = "${module.example_team_es.kibana_endpoint}"
+    iam_role_name   = "${module.example_team_es.iam_role_name}"
+    iam_role_arn    = "${module.example_team_es.iam_role_arn}"
+  }
 }
