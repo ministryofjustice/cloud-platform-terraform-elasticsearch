69f793c72284ee53385d52002aff82855a2b1cfe
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2019-12-05 15:34:48 +0000

    Removing variables related to logs as users wont be able to use it (#5)

--- variables.tf
+++ variables.tf
@@ -94,42 +94,6 @@ variable "ebs_iops" {
   description = "The baseline input/output (I/O) performance of EBS volumes attached to data nodes. Applicable only for the Provisioned IOPS EBS volume type"
 }
 
-variable "log_publishing_index_enabled" {
-  type        = string
-  default     = "false"
-  description = "Specifies whether log publishing option for INDEX_SLOW_LOGS is enabled or not"
-}
-
-variable "log_publishing_search_enabled" {
-  type        = string
-  default     = "false"
-  description = "Specifies whether log publishing option for SEARCH_SLOW_LOGS is enabled or not"
-}
-
-variable "log_publishing_application_enabled" {
-  type        = string
-  default     = "false"
-  description = "Specifies whether log publishing option for ES_APPLICATION_LOGS is enabled or not"
-}
-
-variable "log_publishing_index_cloudwatch_log_group_arn" {
-  type        = string
-  default     = ""
-  description = "ARN of the CloudWatch log group to which log for INDEX_SLOW_LOGS needs to be published"
-}
-
-variable "log_publishing_search_cloudwatch_log_group_arn" {
-  type        = string
-  default     = ""
-  description = "ARN of the CloudWatch log group to which log for SEARCH_SLOW_LOGS  needs to be published"
-}
-
-variable "log_publishing_application_cloudwatch_log_group_arn" {
-  type        = string
-  default     = ""
-  description = "ARN of the CloudWatch log group to which log for ES_APPLICATION_LOGS needs to be published"
-}
-
 variable "automated_snapshot_start_hour" {
   description = "Hour at which automated snapshots are taken, in UTC"
   default     = 0
