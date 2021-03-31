de3dfe6604e3fd072eb39c20c5c92946f9dcc7bd
Alejandro Garrido Mota <alejandro.garrido@digital.justice.gov.uk>
2021-03-31 13:45:07 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,10 +7,6 @@ variable "cluster_name" {
   description = "The name of the cluster (eg.: cloud-platform-live-0)"
 }
 
-variable "cluster_state_bucket" {
-  description = "The name of the S3 bucket holding the terraform state for the cluster"
-}
-
 variable "team_name" {
   description = "The name of your development team"
 }
