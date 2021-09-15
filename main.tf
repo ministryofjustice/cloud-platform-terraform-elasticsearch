8200ec121a8de80a380410727438db672a8f91c7
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-09-15 15:35:11 +0100

    Update eks_cluster_oidc_issuer_url
    This is to fix error:
    This value does not have any attributes.

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ locals {
   elasticsearch_domain_name    = "${var.team_name}-${var.environment-name}-${var.elasticsearch-domain}"
   aws_es_irsa_sa_name          = var.irsa_enabled ? var.aws_es_irsa_sa_name : null
   assume_role_name             = var.assume_enabled ? local.identifier : null
-  eks_cluster_oidc_issuer_url  = var.irsa_enabled ? data.aws_eks_cluster.live.identity.oidc.issuer : null
+  eks_cluster_oidc_issuer_url  = var.irsa_enabled ? data.aws_eks_cluster.live.identity[0].oidc[0].issuer : null
   es_domain_policy_identifiers = var.assume_enabled ? aws_iam_role.elasticsearch_role[0].arn : module.iam_assumable_role_irsa_elastic_search.this_iam_role_arn
 }
 
